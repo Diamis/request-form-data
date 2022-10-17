fb924d3f9e65103455073309a31b181ec5d3e026
Med Aleksey <amed@ibs.ru>
2022-10-17 18:02:49 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { UploadOutlined } from "@ant-design/icons";
+import { Form, Button, Upload, Input } from "antd";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [fileList, setFileList] = useState([]);
+  const [form] = Form.useForm();
+
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+
+  const onReset = () => form.resetFields();
+
+  const handleSubmit = (values) => {
+    const { url, token, product } = values;
+    const formData = new FormData();
+    const files = fileList.map((file) => file.originFileObj);
+
+    formData.append("images", files);
+    formData.append("product", JSON.stringify(product));
+
+    console.log("values", values);
+    console.log("files", files);
+
+    axios({
+      url,
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    })
+      .then(console.log)
+      .catch(console.error);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Form
+        form={form}
+        onFinish={handleSubmit}
+        style={{ width: "100%", maxWidth: "480px" }}
+        initialValues={{
+          url: "http://localhost:5675/api/0.2/flyers/product",
+        }}
+      >
+        <Form.Item label="URL" name="url">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Token" name="token">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Product" name="product">
+          <Input.TextArea />
+        </Form.Item>
+        <br />
+        <Upload
+          listType="picture"
+          defaultFileList={[...fileList]}
+          onChange={handleChange}
         >
-          Learn React
-        </a>
-      </header>
+          <Button icon={<UploadOutlined />}>Upload</Button>
+        </Upload>
+        <br />
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Request
+          </Button>
+          <Button htmlType="button" onClick={onReset}>
+            Reset
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
-}
+};
 
 export default App;
